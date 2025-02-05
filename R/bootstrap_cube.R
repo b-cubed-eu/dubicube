f2d5c3d12f2f81046d35f42544cc5ea1153d8e67
Ward Langeraert <ward.langeraert@inbo.be>
2025-02-05 10:28:29 +0100

    add details and references

--- R/bootstrap_cube.R
+++ R/bootstrap_cube.R
@@ -36,6 +36,66 @@
 #'   deviation of the bootstrap replicates per group)
 #'   - `bias_boot`: The bias of the bootstrap estimate per group
 #'
+#' @details
+#' Bootstrapping is a statistical technique used to estimate the distribution of
+#' a statistic by resampling with replacement from the original data
+#' (Davison & Hinkley, 1997; Efron & Tibshirani, 1994).
+#' In the case of data cubes, each row is sampled with replacement.
+#' Below are the common notations used in bootstrapping:
+#'
+#' 1. **Original Sample Data**: \eqn{\mathbf{X} = \{X_1, X_2, \ldots, X_n\}}
+#'    - The initial set of observed data points. Here, \eqn{n} is the sample
+#'    size. This corresponds to the number of cells in a data cube or the number
+#'    of rows in tabular format.
+#'
+#' 2. **Statistic of Interest**: \eqn{\theta}
+#'    - The parameter or statistic being estimated, such as the mean
+#'    \eqn{\bar{X}}, variance \eqn{\sigma^2}, or a biodiversity indicator. Let
+#'    \eqn{\hat{\theta}} denote the estimated value of \eqn{\theta}
+#'    calculated from the complete dataset \eqn{\mathbf{X}}.
+#'
+#' 3. **Bootstrap Sample**: \eqn{\mathbf{X}^* = \{X_1^*, X_2^*, \ldots, X_n^*\}}
+#'    - A sample of size \eqn{n}
+#'    drawn with replacement from the original sample
+#'    \eqn{\mathbf{X}}. Each \eqn{X_i^*} is drawn independently from
+#'    \eqn{\mathbf{X}}.
+#'    - A total of \eqn{B} bootstrap samples are drawn from the original data.
+#'    Common choices for \eqn{B} are 1000 or 10,000 to ensure a good
+#'    approximation of the distribution of the bootstrap replications (see
+#'    further).
+#'
+#' 4. **Bootstrap Replication**: \eqn{\hat{\theta}^*_b}
+#'    - The value of the statistic of interest calculated from the \eqn{b}-th
+#'    bootstrap sample \eqn{\mathbf{X}^*_b}. For example, if \eqn{\theta} is
+#'    the sample mean, \eqn{\hat{\theta}^*_b = \bar{X}^*_b}.
+#'
+#' 5. **Bootstrap Statistics**:
+#'
+#' - **Boostrap Estimate of the Statistic**: \eqn{\hat{\theta}_{\text{boot}}}
+#'   - The average of the bootstrap replications:
+#'
+#' \deqn{\hat{\theta}_{\text{boot}} = \frac{1}{B} \sum_{b=1}^B \hat{\theta}^*_b}
+#'
+#' - **Bootstrap Bias**: \eqn{\text{Bias}_{\text{boot}}}
+#'    - This bias indicates how much the bootstrap estimate deviates from the
+#'    original sample estimate. It is calculated as the difference between the
+#'    average bootstrap estimate and the original estimate:
+#'
+#' \deqn{\text{Bias}_{\text{boot}} = \frac{1}{B} \sum_{b=1}^B (\hat{\theta}^*_b
+#' - \hat{\theta}) = \hat{\theta}_{\text{boot}} - \hat{\theta}}
+#'
+#' - **Bootstrap Standard Error**: \eqn{\text{SE}_{\text{boot}}}
+#'    - The standard deviation of the bootstrap replications, which estimates
+#'    the variability of the statistic.
+#'
+#' @references
+#' Davison, A. C., & Hinkley, D. V. (1997). Bootstrap Methods and their
+#' Application (1st ed.). Cambridge University Press.
+#' \url{https://doi.org/10.1017/CBO9780511802843}
+#'
+#' Efron, B., & Tibshirani, R. J. (1994). An Introduction to the Bootstrap
+#' (1st ed.). Chapman and Hall/CRC. \url{https://doi.org/10.1201/9780429246593}
+#'
 #' @export
 #'
 #' @import dplyr

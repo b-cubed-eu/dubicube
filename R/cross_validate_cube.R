92b2a737b05a67047f795074a4bb4de9cd2cb0ed
Ward Langeraert <ward.langeraert@inbo.be>
2025-02-13 10:48:22 +0100

    improve general documentation

--- R/cross_validate_cube.R
+++ R/cross_validate_cube.R
@@ -1,10 +1,11 @@
 #' Leave-one-out cross-validation for data cubes
 #'
 #' This function performs leave-one-out (LOO) or k-fold (experimental)
-#' cross-validation (CV) on a biodiversity data cube to assess the performance of a specified
-#' indicator function over time. It partitions the data by species, calculates
-#' the specified indicator on training data, and compares it with the true
-#' values to evaluate the influence of one or more species on the final result.
+#' cross-validation (CV) on a biodiversity data cube to assess the performance
+#' of a specified indicator function. It partitions the data by a specified
+#' variable, calculates the specified indicator on training data, and compares
+#' it with the true values to evaluate the influence of one or more categories
+#' on the final result.
 #'
 #' @param data_cube A data cube object (class 'processed_cube' or 'sim_cube',
 #' see `b3gbi::process_cube()`) or a dataframe (from `$data` slot of
@@ -13,26 +14,42 @@
 #' statistic(s) of interest. This function must return a dataframe with a column
 #' `diversity_val` containing the statistic of interest.
 #' @param ... Additional arguments passed on to `fun`.
-#' @param grouping_var A string specifying the grouping variable(s) for the
-#' bootstrap analysis. The output of `fun(data_cube)` returns a row per group.
+#' @param grouping_var A string specifying the grouping variable(s) for `fun`.
+#' The output of `fun(data_cube)` returns a row per group.
+#' @param out_var A string specifying the column by which the data should be
+#' left out iteratively. Default is `"taxonKey"` which can be used for
+#' leave-one-species-out CV.
 #' @param crossv_method Method of data partitioning.
-#' If `crossv_method = "loo"` (default), `S = number of species` training
-#' partitions are created containing `S - 1` rows each.
+#' If `crossv_method = "loo"` (default),
+#' `S = number of unique values in out_var` training partitions are created
+#' containing `S - 1` rows each.
 #' If `crossv_method = "kfold"`, the aggregated data is split the data into
-#' `k` exclusive partitions containing `S / k` rows each.
+#' `k` exclusive partitions containing `S / k` rows each. K-fold CV is
+#' experimental and results should be interpreted with caution.
 #' @param k Number of folds (an integer). Used only if
 #' `crossv_method = "kfold"`. Default 5.
+#' @param progress Logical. Whether to show a progress bar. Set to `TRUE` to
+#' display a progress bar, `FALSE` (default) to suppress it.
+#' @param max_out_cats An integer specifying the maximum number of unique
+#' categories in `out_var` to leave out iteratively. Default is `1000`.
+#' This can be increased if needed, but higher values may significantly increase
+#' runtime.
 #'
-#' @returns A dataframe summarizing the cross-validation results, including:
-#' - Cross-Validation id (`id_cv`)
-#' - The grouping variable (e.g., year)
-#' - The species left out during each cross-validation iteration
-#' - The computed statistic values for both training (`rep_cv`) and true
+#' @returns A dataframe containing the cross-validation results with the
+#' following columns:
+#'   - Cross-Validation id (`id_cv`)
+#'   - The grouping variable `grouping_var` (e.g., year)
+#'   - The category left out during each cross-validation iteration
+#'   (`cat_left_out`)
+#'   - The computed statistic values for both training (`rep_cv`) and true
 #' datasets (`est_original`)
-#' - Error metrics: error (= difference), squared error, absolute difference,
-#' relative difference, and percent difference
-#' - Error metrics summarised by `grouping_var`: mean relative difference
-#' (`mre`), mean squared error (`mse`) and root mean squared error (`rmse`).
+#'   - Error metrics: error (`error`), squared error (`sq_error`),
+#'   absolute difference (`abs_diff`), relative difference (`rel_diff`), and
+#'   percent difference (`perc_diff`)
+#'   - Error metrics summarised by `grouping_var`: mean relative difference
+#' (`mre`), mean squared error (`mse`) and root mean squared error (`rmse`)
+#'
+#' See Details section on how these error metrics are calculated.
 #'
 #' @details
 #' Details here
@@ -49,13 +66,14 @@
 #' @examples
 #' # Example here
 
-
 cross_validate_cube <- function(
     data_cube,
     fun,
     grouping_var,
+    out_var = "taxonKey",
     crossv_method = c("loo", "kfold"),
     k = ifelse(crossv_method == "kfold", 5, NA),
+    max_out_cats = 1000,
     progress = FALSE) {
 
   # Define cross-validation function
@@ -87,7 +105,7 @@ cross_validate_cube <- function(
     # Get species left out
     species_df <- data.frame(
       id_cv = seq_along(taxon_list),
-      species_left_out = taxon_list
+      cat_left_out = taxon_list
     )
   } else {
     # Species partitioning
@@ -96,7 +114,7 @@ cross_validate_cube <- function(
       modelr::crossv_kfold(id = "id_cv", k = k)
 
     # Get species left out
-    species_left_out_list <- lapply(lapply(taxon_list$test, as.integer),
+    cat_left_out_list <- lapply(lapply(taxon_list$test, as.integer),
                                     function(indices) {
                                       df <- data_cube$data %>%
                                         distinct(.data$taxonKey)
@@ -105,15 +123,15 @@ cross_validate_cube <- function(
                                         pull(.data$taxonKey)
                                     }
     )
-    names(species_left_out_list) <- NULL
+    names(cat_left_out_list) <- NULL
 
     species_df <- tibble(
       id_cv = as.numeric(taxon_list$id_cv),
-      species_left_out = species_left_out_list
+      cat_left_out = cat_left_out_list
     )
 
     # Create cross validation datasets
-    cv_datasets <- lapply(species_left_out_list, function(taxa) {
+    cv_datasets <- lapply(cat_left_out_list, function(taxa) {
       data_cube$data[!data_cube$data$taxonKey %in% taxa, ]
     })
   }
@@ -148,7 +166,7 @@ cross_validate_cube <- function(
       rmse = sqrt(.data$mse),
       .by = all_of(grouping_var)) %>%
     dplyr::arrange(.data[[grouping_var]]) %>%
-    dplyr::select("id_cv", all_of(grouping_var), "species_left_out", "rep_cv",
+    dplyr::select("id_cv", all_of(grouping_var), "cat_left_out", "rep_cv",
                   "est_original", dplyr::everything())
 
   return(out_df)

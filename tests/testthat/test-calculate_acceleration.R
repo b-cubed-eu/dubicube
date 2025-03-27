ca1e1aff8324eb397d60405c49d584d87fc756a4
Ward Langeraert <ward.langeraert@inbo.be>
2025-03-27 13:55:13 +0100

    add unit tests

--- tests/testthat/test-calculate_acceleration.R
+++ tests/testthat/test-calculate_acceleration.R
@@ -71,3 +71,66 @@ acceleration_df4 <- calculate_acceleration(
   grouping_var = c("year", "taxonKey"),
   ref_group = ref_year,
   influence_method = "usual")
+
+example_results <- list(
+  acceleration_df1, acceleration_df2, acceleration_df3, acceleration_df4)
+
+## Perform tests
+# Test calculate_acceleration output
+test_that("calculate_acceleration returns a df with expected structure", {
+  # Data frame
+  lapply(example_results, function(df) {
+    expect_s3_class(df, "data.frame")
+  })
+
+  # Correct column names
+  lapply(example_results, function(df) {
+    expect_true(all(
+      c("year", "taxonKey", "acceleration") %in% names(df)
+      ))
+  })
+
+  # Correct number of columns
+  expect_true(nrow(acceleration_df1) == length(species) * length(years))
+  expect_true(nrow(acceleration_df2) == length(species) * length(years))
+  expect_true(nrow(acceleration_df3) == length(species) * (length(years) - 1))
+  expect_true(nrow(acceleration_df4) == length(species) * (length(years) - 1))
+})
+
+# Test whether processed cube and dataframe result in the same
+test_that("Identical results for processed cube and dataframe", {
+  # Input dataframe
+  acceleration_df12 <- calculate_acceleration(
+    data_cube = cube_df,
+    fun = mean_obs,
+    grouping_var = c("year", "taxonKey"),
+    influence_method = "pos")
+
+  # Input 'processed_cube'
+  acceleration_df22 <- calculate_acceleration(
+    data_cube = processed_cube,
+    fun = mean_obs_processed,
+    grouping_var = c("year", "taxonKey"),
+    influence_method = "usual")
+
+  # Input dataframe with reference group
+  acceleration_df32 <- calculate_acceleration(
+    data_cube = cube_df,
+    fun = mean_obs,
+    grouping_var = c("year", "taxonKey"),
+    ref_group = ref_year,
+    influence_method = "usual")
+
+  # Input 'processed_cube' with reference group
+  acceleration_df42 <- calculate_acceleration(
+    data_cube = processed_cube,
+    fun = mean_obs_processed,
+    grouping_var = c("year", "taxonKey"),
+    ref_group = ref_year,
+    influence_method = "pos")
+
+  expect_identical(acceleration_df2, acceleration_df12)
+  expect_identical(acceleration_df1, acceleration_df22)
+  expect_identical(acceleration_df4, acceleration_df32)
+  expect_identical(acceleration_df3, acceleration_df42)
+})
